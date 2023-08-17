7734c61b378adb3c139d6d59f29a48d3334eb3e4
wingunkh <99gusrms@naver.com>
2023-08-17 14:53:33 +0900

    2343

---
+++ 05 탐색/05-3 이진 탐색/030 블루레이 만들기.py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+a.sort
+start = max(a)
+end = sum(a)
+
+while start <= end:
+    pc = (start + end) // 2
+    sum = 0
+    count = 0
+
+    for i in a:
+        if sum + i > pc:
+            count += 1
+            sum = 0
+        sum += i
+    if sum != 0:
+        count += 1
+
+    if count > m:
+        start = pc + 1
+    else:
+        end = pc - 1
+
+print(start)
