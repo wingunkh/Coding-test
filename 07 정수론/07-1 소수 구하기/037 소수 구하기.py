bec55b57483b484862bd9a4ce5b70fe8935edf12
wingunkh <99gusrms@naver.com>
2023-08-25 15:37:12 +0900

    1929

--- 07 정수론/07-1 소수 구하기/037 소수 구하기.py
+++ 07 정수론/07-1 소수 구하기/037 소수 구하기.py
@@ -1,14 +1,15 @@
-def prime_number(a):
-    if a == 1:
-        return 0
-        
-    for i in range(2, int(a**0.5)+1, 1):
-        if a % i == 0:
-            break
-    else:
-        print(a)
-
 n, m = map(int, input().split())
+a = [0] * (m+1)
+
+for i in range(2, m+1):
+    a[i] = i
+
+for i in range(2, int(m ** 0.5)+1):
+    if a[i] == 0:
+        continue
+    for j in range(i+i, m+1, i):
+        a[j] = 0
 
 for i in range(n, m+1):
-    prime_number(i)
+    if a[i]:
+        print(a[i])
