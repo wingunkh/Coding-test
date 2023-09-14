23293bb67366a1ff43baed452e23bf49fa22d9fe
wingunkh <99gusrms@naver.com>
2023-09-14 10:35:09 +0900

    1916

---
+++ 08 그래프/08-4 다익스트라/057 최소 비용 구하기.py
@@ -0,0 +1,33 @@
+import sys
+from queue import PriorityQueue
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+a = [[] for _ in range(n+1)]
+distance = [sys.maxsize for _ in range(n+1)]
+visited = [False for _ in range(n+1)]
+q = PriorityQueue()
+
+for i in range(m):
+    s, e, w = map(int, input().split())
+    a[s].append((e, w))
+
+start, end = map(int, input().split())
+
+q.put((0, start))
+distance[start] = 0
+
+while q.qsize() > 0:
+    _, now = q.get()
+    if visited[now]:
+        continue
+    visited[now] = True
+
+    for tmp in a[now]:
+        next, weight = tmp
+        if distance[next] > distance[now] + weight:
+            distance[next] = distance[now] + weight
+            q.put((distance[next], next))
+
+print(distance[end])
