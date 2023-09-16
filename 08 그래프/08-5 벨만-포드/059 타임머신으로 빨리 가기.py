a1f08bdfc42262d6f7963ccb2be15619e2c30d9f
wingunkh <99gusrms@naver.com>
2023-09-16 14:32:47 +0900

    11657

--- 08 그래프/08-5 벨만-포드/059 타임머신으로 빨리 가기.py
+++ 08 그래프/08-5 벨만-포드/059 타임머신으로 빨리 가기.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+
 n, m = map(int, input().split())
 edges = []
 distance = [sys.maxsize for _ in range(n+1)]
@@ -11,7 +12,9 @@ for i in range(m):
 
 distance[1] = 0
 
-for _ in range(n-1): # 특정 두 노드의 최단거리를 구성할 수 있는 엣지의 최대 개수 = 노드 개수 - 1
+# 벨만-포드 알고리즘에서 최단 거리 리스트 업데이트 반복 횟수는 노드 개수 -1이다.
+# (특정 두 노드의 최단거리를 구성할 수 있는 엣지의 최대 개수 = 노드 개수 - 1)
+for _ in range(n-1):
     for s, e, t in edges:
         if distance[s] != sys.maxsize and distance[e] > distance[s] + t:
             distance[e] = distance[s] + t
