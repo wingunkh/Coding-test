6d2464aa5790c552acf307d6ed0f32f34ed03568
wingunkh <99gusrms@naver.com>
2023-09-25 12:40:49 +0900

    1991

--- 09 트리/09-3 이진 트리/070 트리 순회하기.py
+++ 09 트리/09-3 이진 트리/070 트리 순회하기.py
@@ -1,23 +1,23 @@
-n = int(input())
-tree = {}
-
-def preorder(v):
+def preorder(v): # 전위 순회 : 루트 -> 왼쪽 자식 -> 오른쪽 자식
     if v != '.':
         print(v, end = '')
         preorder(tree[v][0])
         preorder(tree[v][1])
 
-def inorder(v):
+def inorder(v): # 중위 순회 : 왼쪽 자식 -> 루트 -> 오른쪽 자식
     if v != '.':
         inorder(tree[v][0])
         print(v, end = '')
         inorder(tree[v][1])
 
-def postorder(v):
+def postorder(v): # 후위 순회 : 왼쪽 자식 -> 오른쪽 자식 -> 루트
     if v != '.':
         postorder(tree[v][0])
         postorder(tree[v][1])
         print(v, end = '')
+
+n = int(input())
+tree = {}
         
 for i in range(n):
     root, left, right = input().split()
