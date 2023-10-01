282ad3381b5641e038be502b2b59d8747d1f2e59
wingunkh <99gusrms@naver.com>
2023-10-01 11:31:10 +0900

    11050

--- 10 조합/076 이항계수 구하기 1.py
+++ 10 조합/076 이항계수 구하기 1.py
@@ -2,8 +2,8 @@ n, k = map(int, input().split())
 d = [[0 for _ in range(n+1)] for _ in range(n+1)]
 
 for i in range(n+1):
-    d[i][1] = i # i개 중 1개를 뽑는 경우의 수는 i개
     d[i][0] = 1 # i개 중 1개도 선택하지 않는 경우의 수는 1개
+    d[i][1] = i # i개 중 1개를 뽑는 경우의 수는 i개
     d[i][i] = 1 # i개 중 i개를 선택하는 경우의 수는 1개 
 
 for i in range(n+1):
