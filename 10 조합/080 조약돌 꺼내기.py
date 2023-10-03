ffbedf7b5153bbda0c5e8c1d677b6f818691b8a3
wingunkh <99gusrms@naver.com>
2023-10-03 15:05:48 +0900

    13251

--- 10 조합/080 조약돌 꺼내기.py
+++ 10 조합/080 조약돌 꺼내기.py
@@ -1,10 +1,10 @@
 m = int(input())
 d = list(map(int, input().split()))
 prob = [0 for _ in range(m)]
-answer = 0
-
 k = int(input())
+
 amount = sum(d)
+answer = 0
 
 for i in range(m):
     if d[i] >= k:
